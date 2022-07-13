aa68bbc6c9c38059024040f285aa0775d0ef3e16
sidharthbajpai <sidharthbajpaiwork@gmail.com>
2022-07-13 19:50:54 +0530

    First Commit

---
+++ Online-Medicine-Shopping-System/src/main/java/com/capg/OnlineMedicineShoppingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMedicineShoppingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMedicineShoppingSystemApplication.class, args);
+	}
+
+}
---
+++ Online-Medicine-Shopping-System/src/test/java/com/capg/OnlineMedicineShoppingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMedicineShoppingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
