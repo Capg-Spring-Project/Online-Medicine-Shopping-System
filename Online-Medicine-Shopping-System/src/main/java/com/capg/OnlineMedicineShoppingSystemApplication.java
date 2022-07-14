868940ebd2846726f5c97d0be0fb74217ed32545
teambootcamp <avijitpaul.16july@gmail.com>
2022-07-14 15:38:38 +0530

    trial

--- Online-Medicine-Shopping-System/src/main/java/com/capg/OnlineMedicineShoppingSystemApplication.java
+++ Online-Medicine-Shopping-System/src/main/java/com/capg/OnlineMedicineShoppingSystemApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineMedicineShoppingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineMedicineShoppingSystemApplication.class, args);
+		SpringApplication.run(OnlineMedicineShoppingSystemApplication.class, 
+				args);
 	}
 
 }
