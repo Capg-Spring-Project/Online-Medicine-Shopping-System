05e833fe490cfee2de605e5930e8e40367d9b16e
avijitpaul16 <avijitpaul.16july@gmail.com>
2022-07-14 09:31:37 +0530

    test commite

--- Online-Medicine-Shopping-System/src/main/java/com/capg/entity/Admin.java
+++ Online-Medicine-Shopping-System/src/main/java/com/capg/entity/Admin.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+// Edited
 @Entity
 public class Admin {
 	@Id
